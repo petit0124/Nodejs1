23fc25437760c171fd7331c5f2164a522a9e474a
旭聪 <>
2023-02-27 06:37:23 +0000

    Initial commit

---
+++ fun2.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reduce: function(a, b) {
+    console.log("我是fun2的reduce方法");
+    console.log(a - b);
+  },
+  add: function(a, b) {
+    console.log("我是fun2的add方法");
+    console.log(a + b);
+  }
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var fun2 = require('./fun2');
+const app = express();
+fun2.add(1, 2);
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
