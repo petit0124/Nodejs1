4c6a5b94baf9279e43deb86f91a2e3cf95a4d506
Scott Chen <petit_82@sina.com>
2023-03-07 18:21:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const express = require('axios');
 
 var fun2 = require('./fun2');
 const app = express();
-<<<<<<< HEAD
-fun2.add(1, 5);
-=======
 
 fun2.add(1, 3);
->>>>>>> f02a729 (test)
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
