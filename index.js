df68bb6f0ab1d319b5f501029066ee6307403364
Scott Chen <petit_82@sina.com>
2023-03-07 18:20:42 +0800

    test

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require('axios');
 
 var fun2 = require('./fun2');
 const app = express();
+<<<<<<< HEAD
 fun2.add(1, 5);
+=======
+
+fun2.add(1, 3);
+>>>>>>> f02a729 (test)
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
