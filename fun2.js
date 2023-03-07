3fa71fc27dfb46fde7656cd5548c2bfcada094fd
Scott Chen <petit_82@sina.com>
2023-03-07 15:43:11 +0800

    Update fun2.js

--- fun2.js
+++ fun2.js
@@ -1,10 +1,10 @@
 module.exports = {
   reduce: function(a, b) {
-    console.log("我是fun2的reduce方法eee");
+    console.log("我是fun2的reduce方法");
     console.log(a - b);
   },
   add: function(a, b) {
-    console.log("我是fun2的add方法fff");
+    console.log("我是fun2的add方法");
     console.log(a + b);
   }
 }
